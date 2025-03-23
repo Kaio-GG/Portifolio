d8bec5be9ebbf6c586be06e260893422999d2aea
Kaio <kaiofdss@gmail.com>
2025-03-23 13:57:49 -0300

    responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <img src='/assets/image/logo-kaio.svg' alt='' className='logo'/>
       </header>
       <section className='faixa1'>
-        <h1>Desenvolvedor Backend, Frontend, Cientista de Dados</h1>
+        <h1>Desenvolvedor FullStack</h1>
         <div>
           <h3>“A arte de programar consiste na arte de organizar e dominar a complexidade.”</h3>
           <img src='/assets/image/carinha_no_pc_sem_fundo.png' alt='' className='imagem-faixa1' />
@@ -22,22 +22,24 @@ function App() {
       </section>
       <section className='faixa2'>
         <h1>About Me</h1>
-        <div>
+        <div className='org'>
           <h3>Kaio Felipe Dias de Souza Sant'Ana</h3>
           <p>
-            Sou formado em cursos técnicos de Informática e Língua Inglesa, e atualmente estou cursando Análise e Desenvolvimento de Sistemas. Sou apaixonado pelo desenvolvimento de software e, durante minha jornada acadêmica, adquiri habilidades técnicas sólidas e demonstrei comprometimento ao enfrentar desafios complexos.
+              Sou um desenvolvedor FullStack apaixonado por tecnologia e inovação. Me formei em um curso técnico de informática com foco em desenvolvimento, onde descobri meu interesse por criar soluções que impactam o dia a dia das pessoas. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas na Universidade Ítalo, buscando aprofundar ainda mais meus conhecimentos na área. Busco constantemente desafios para aprimorar minhas habilidades e desenvolver aplicações úteis e intuitivas. Tenho experiência com JavaScript, TypeScript, React, Express.js e MySQL, além de estar sempre aberto a aprender novas tecnologias.             
           </p>
         </div>
       </section>
       <section className='faixa3'>
         <h1>Linguagens, Frameworks e Banco de Dados</h1>
         <div className='org-imagens'>
+          
           <div>
             <img src='/assets/image/javascript-logo.svg' alt='JavaScript'/>
           </div>
           <div>
             <img src='/assets/image/typescript.svg' alt='TypeScript'/>           
           </div>
+
           <div>
             <img src='/assets/image/mysql-logo.svg' alt='MySQL'/>          
           </div>
@@ -50,7 +52,7 @@ function App() {
       <section className='faixa4'>
         <h1>Projetos</h1>
         <div className='meio'>
-              <Card nome='Chat' texto='Desenvolvimento de um chat com funcionalidades de comunicação instantânea.' />
+              <Card nome='Chat' texto='Desenvolvimento de um chat com funcionalidades de comunicação instantânea.'  />
               <Card nome='MyWorkship' texto='Trabalho em equipe para criar um sistema de agendamento eficiente e intuitivo.' />
               <Card nome='Biblioteca ' texto='Criação de um site para gerenciamento de livros, permitindo aos usuários explorar e administrar coleções de livros.' />
               <Card nome='StockExpert' texto='Desenvolvimento de um sistema de administração de vendas, onde os usuários podem gerenciar suas transações e inventário' />        
@@ -60,13 +62,15 @@ function App() {
         <h1>Formação</h1>
         <div className='meio-faixa'>
           <div className='meio'>
-              <img src='/assets/image/NSF.png' alt='' />
-              <h4>- INFORMATICA</h4>
-              <h4>- INGLES</h4>
+              <img className='icon-nfs' src='/assets/image/NSF.png' alt='' />
+              <div className='org-cell'>
+                  <h5 className='texto-info'>- INFORMATICA</h5>
+                  <h5 className='texto-info'>- INGLES</h5>
+              </div>
           </div>
           <div className='meio'>
-              <img src='/assets/image/Cópia-de-centro-universitário-catolico-italo-brasileiro-letras-brancas-horizontal.png' alt='' />
-              <h4>- ANALISE E DESENVOLVEMENTO DE SISTEMA</h4>
+              <img className='icon-italo' src='/assets/image/Cópia-de-centro-universitário-catolico-italo-brasileiro-letras-brancas-horizontal.png' alt='' />
+              <h5>- ANALISE E DESENVOLVEMENTO DE SISTEMA</h5>
           </div>
         </div>
       </section>
